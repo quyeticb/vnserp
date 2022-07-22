08166b9e176bea8914c112dbe1da1e8b641c3aa0
quyeticb <quyeticb@gmail.com>
2022-07-22 12:16:14 +0700

    Update build-php-centos.sh

--- build-php-centos.sh
+++ build-php-centos.sh
@@ -73,6 +73,7 @@ make && make install
 echo "extension=solr
 extension=xmlrpc.so" > /etc/php8z/conf.d/custom.ini
 rm -Rf *
-
+echo "net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
 
 
